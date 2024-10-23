7c308695bf728d775af5bba03adf5ab8884b681f
Shreyas Ladhe <112794761+ShreyasLadhe@users.noreply.github.com>
2024-10-23 19:56:16 +0530

    Updated binarybandit file

--- Lab-7/binbandepsilon.py
+++ Lab-7/binbandepsilon.py
@@ -1,28 +1,70 @@
-import numpy as np
+import random
+import matplotlib.pyplot as plt
 
-class EpGrAgent:
-    def __init__(self, n_arms, epsilon):
-        self.n_arms = n_arms
-        self.epsilon = epsilon
-        self.q_values = np.zeros(n_arms)  
-        self.action_counts = np.zeros(n_arms)  
+random.seed(10)
 
-    def sel_act(self):
-        if np.random.rand() < self.epsilon:  
-            return np.random.randint(self.n_arms)
-        else:  
-            return np.argmax(self.q_values)
+class BinaryBandit:
+    def __init__(self):  
+        self.num_arms = 2  
 
-    def update_qval(self, action, reward):
-        self.action_counts[action] += 1
-        self.q_values[action] += (reward - self.q_values[action]) / self.action_counts[action]
+    def avail_act(self):
+        return list(range(self.num_arms))
 
-binband = [0.1, 0.2] 
-agent = EpGrAgent(n_arms=2, epsilon=0.1)
+    def binBandA(self, action):
+        probabilities = [0.1, 0.2] 
+        value = random.random()
+        return 1 if value < probabilities[action] else 0
 
-for _ in range(1000):  
-    action = agent.sel_act()
-    reward = np.random.rand() < binband[action] 
-    agent.update_qval(action, reward)
+    def binBandB(self, action):
+        probabilities = [0.8, 0.9]  
+        value = random.random()
+        return 1 if value < probabilities[action] else 0
 
-print("Estimated Q values:", agent.q_values)
+def ep_greedy(bandit, epsilon, iterations, use_banditA=True):
+    q_val = [0] * bandit.num_arms  
+    act_count = [0] * bandit.num_arms  
+    tot_rewards = []
+    avg_rewards = [0]
+
+    for iteration in range(iterations):
+        if random.random() > epsilon:
+            chosen_action = q_val.index(max(q_val)) 
+        else:
+            chosen_action = random.choice(bandit.avail_act())  
+
+        if use_banditA:
+            reward = bandit.binBandA(chosen_action)
+        else:
+            reward = bandit.binBandB(chosen_action)
+        
+        tot_rewards.append(reward)
+        act_count[chosen_action] += 1
+        q_val[chosen_action] += (reward - q_val[chosen_action]) / act_count[chosen_action]
+        avg_rewards.append(avg_rewards[-1] + (reward - avg_rewards[-1]) / (iteration + 1))
+
+    return q_val, avg_rewards, tot_rewards
+
+
+random.seed(10)
+bandit = BinaryBandit()
+
+Q_A, avg_rewards_A, rewards_A = ep_greedy(bandit, 0.2, 2000, use_banditA=True)
+Q_B, avg_rewards_B, rewards_B = ep_greedy(bandit, 0.2, 2000, use_banditA=False)
+
+fig, (axis1, axis2) = plt.subplots(1, 2, figsize=(15, 5))
+
+axis1.plot(avg_rewards_A, label="Bandit A")
+axis1.plot(avg_rewards_B, label="Bandit B")
+axis1.title.set_text("Average Rewards vs Iterations")
+axis1.set_xlabel("Iterations")
+axis1.set_ylabel("Average Reward")
+axis1.legend()
+
+axis2.plot(rewards_A, label="Bandit A")
+axis2.plot(rewards_B, label="Bandit B")
+axis2.title.set_text("Rewards per Iteration")
+axis2.set_xlabel("Iterations")
+axis2.set_ylabel("Reward")
+axis2.legend()
+
+plt.show()
